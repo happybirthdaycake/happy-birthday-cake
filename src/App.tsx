b2f4bc7af397afef90bd546b897a4ed1727dbdd1
Your Cake <happy.birthday210783@gmail.com>
2022-08-14 13:51:03 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import "./App.css";
 
-import reactLogo from "./assets/react.svg";
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite. svg";
 
 function App() {
   const [count,  setCount] = useState(0)
   return (  
     <>              
-      <div>    
-         <href="https://vitejs.dev" target="_blank">
+      <div>  
+          
+         <a href="https://vitejs.dev" target="_blank">
              <img  src={viteLogo} className="logo" alt="Vite logo" />
         </ a>
         <a hr ef= "https://react.dev" target="_blank">
