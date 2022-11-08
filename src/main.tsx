027ac5edc406700742c667f47999e43c8ba87d25
Your Cake <happy.birthday210783@gmail.com>
2022-11-08 12:12:26 +0100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import App from "./App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
- 
+ReactDOM.createRoot(document.getElementById("root")!).render( 
  <React.StrictMode>
     <App />
   </React.StrictMode>
