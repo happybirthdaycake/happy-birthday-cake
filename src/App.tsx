aaed2e7366e930559b2de7e0f4e82763255f1231
Your Cake <happy.birthday210783@gmail.com>
2022-12-31 12:30:09 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
   return (   
     <>             
       <div>   
-        <a href="https://vitejs.dev" target="_blank">
+        < href="https://vitejs.dev" target="_blank">
           <img  src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        </ a>
         <a hr ef="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         < /a>  
