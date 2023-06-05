e9d7cb2d98af7e3f13b57659d9766136056e3320
Your Cake <happy.birthday210783@gmail.com>
2023-06-05 10:34:57 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import viteLogo from "/vite.svg";
 function App() {
   const [count, setCount] = useState(0);
   const birthdaycakE = [0, 2, 7, 9, 10, 11, 12,13,14,15,16];
-  const birthdaycaKe = [44,43];
+  const birthdaycaKe = [44, 43];
 
   return (
     <>
